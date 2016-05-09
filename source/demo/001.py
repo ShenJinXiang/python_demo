b4676245d3cba13f70bc2668fa4cb8fbd1f7e75a
shenjinxiang <sjx-sword@163.com>
2016-05-09 09:14:12 +0800

    input print list

---
+++ source/demo/001.py
@@ -0,0 +1,11 @@
+# input and print
+name = input("please enter your name:")
+print("hello", name)
+
+x = int(input("enter first number:"))
+y = int(input("enter second number:"))
+def add(num1, num2) :
+	print(num1, "+", num2, "=", (num1 + num2))
+add(x, y)
+print(1024, "*", 768, "=", (1024 * 768))
+input("OK~~~~")
---
+++ source/demo/002.py
@@ -0,0 +1,39 @@
+﻿# 002.py
+# list
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+print("classmates的长度：", len(classmates))
+print("classmates[0]:", classmates[0]);
+print("classmates[1]:", classmates[1]);
+print("classmates[2]:", classmates[2]);
+print("classmates[-1]:", classmates[-1]);
+print("classmates[-2]:", classmates[-2]);
+print("classmates[-3]:", classmates[-3]);
+print("-----------------------------")
+
+print("末尾添加'Adam':")
+classmates.append('Adam')
+print(classmates)
+print("-----------------------------")
+
+print("'1'位置添加'Jack'")
+classmates.insert(1, 'Jack')
+print(classmates)
+print("-----------------------------")
+
+print("pop:")
+print(classmates.pop())
+print(classmates)
+print("-----------------------------")
+
+print("pop(1)")
+print(classmates.pop(1))
+print(classmates)
+print("-----------------------------")
+
+print("classmates[1] = 'Sarah':")
+classmates[1] = 'Sarah'
+print(classmates)
+print("-----------------------------")
+
+input("end~~~")
